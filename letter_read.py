d5a879836d40d2ee465118b60f1a62ee758cb09b
Rahul Vishwakarma <champ0843@gmail.com>
2020-01-31 20:52:13 +0530

    n

--- letter_read.py
+++
@@ -1,6 +0,0 @@
-import textract
-text = textract.process("C:\\Users\\PCCS\\Downloads\\LETTERS.DOCX")
-text = text.decode("utf-8")
-text = text.split("\n")
-filt = filter(None,text)
-text = list(filt)
